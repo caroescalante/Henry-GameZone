41504256fb07c6b3a6c2e98d15da61d024c2a526
caroescalante <caroescalante2521@gmail.com>
2023-04-04 04:50:41 -0300

    deploy back intento 5

--- api/index.js
+++ api/index.js
@@ -2,10 +2,8 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 
-
-
 conn.sync({ force: true }).then(() => {
-  server.listen( () => {
+  server.listen(6604, () => {
     console.log('%s listening at server deploy');
   });
 });
